4418c5e3f4a3aaae3f0e226f43a92ce4e56aa697
XiaoFeng-QWQ <q1432777209@126.com>
2024-07-19 18:31:58 +0800

    更改NProgress位置

--- app/Views/module/user.auth.footer.php
+++ app/Views/module/user.auth.footer.php
@@ -4,6 +4,11 @@
 <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.bootcdn.net/ajax/libs/nprogress/0.2.0/nprogress.min.js"></script>
 <script src="https://cdn.bootcdn.net/ajax/libs/jquery.pjax/2.0.1/jquery.pjax.min.js"></script>
+<script>
+    NProgress.configure({
+        parent: '.user-auth-container'
+    })
+</script>
 <script src="/StaticResources/js/user.auth.js?v=<?php echo FRAMEWORK_VERSION ?>"></script>
 
 </body>
