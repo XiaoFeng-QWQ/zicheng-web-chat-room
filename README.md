1995a0682c8e6baded5ccb6438034d5b70ecaa7c
XiaoFeng-QWQ <q1432777209@126.com>
2025-01-11 22:30:02 +0800

    兼容最新版

--- README.md
+++ README.md
@@ -1,123 +1,29 @@
 <center>
     <h1>简易聊天室插件</h1>
-    <img src="logo.svg"></img>
+    <img height="400px" src="logo.png"></img>
 </center>
 
-这是一个可以嵌入网页的小型聊天室插件，使用HTML、CSS和JavaScript（依赖jQuery）开发。该插件允许用户在小窗口内接收消息。
+这是一个可以嵌入网页的小型聊天室插件该插件允许用户在小窗口内接收、发送消息。
 
 ## 功能
 
 - **实时聊天**：即可通过API接口实时接收消息。
 - **可折叠聊天窗**：支持展开和折叠功能，方便用户使用。
-- **自适应设计**：在移动设备上默认设置最大宽度与高度，并根据屏幕尺寸进行调整。
 
 ## 使用说明
 
 ### 先决条件
+下载安装PHP程序：https://github.com/XiaoFeng-QWQ/zicheng-web-chat-room/
 
-- 下载安装PHP程序：https://github.com/XiaoFeng-QWQ/zicheng-web-chat-room/
-- 将代码中的`const apiUrl`值替换为你的域名。
-
-### 引入jQuery
-
-请确保在HTML文件中引入最新版本的jQuery：
-
+### 使用
 ```html
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-```
-
-### 插件集成
-
-在HTML页面中插入以下JavaScript代码：
-
-```html
+<script src="js.cookie.min.js"></script>
 <script>
-function ChatHtml() {
-    var container = document.createDocumentFragment();
-    var e_0 = document.createElement("div");
-    var e_1 = document.createElement("style");
-    e_1.appendChild(document.createTextNode("/* 插件样式代码 */"));
-    e_0.appendChild(e_1);
-    var e_2 = document.createElement("div");
-    e_2.setAttribute("class", "collapsed");
-    e_2.setAttribute("id", "ZichenChatRoomHtmlPluginChatbox");
-    var e_3 = document.createElement("div");
-    e_3.setAttribute("class", "chatbox-header");
-    e_3.appendChild(document.createTextNode("\n简易聊天室\n"));
-    var e_4 = document.createElement("a");
-    e_4.setAttribute("href", "https://chat.zicheng.icu/");
-    e_4.setAttribute("target", "_blank");
-    e_4.setAttribute("rel", "noopener noreferrer");
-    e_4.appendChild(document.createTextNode("完整体验地址"));
-    e_3.appendChild(e_4);
-    var e_5 = document.createElement("button");
-    e_5.setAttribute("class", "scrollToBottom");
-    e_5.appendChild(document.createTextNode("返回底部"));
-    e_3.appendChild(e_5);
-    var e_6 = document.createElement("button");
-    e_6.setAttribute("class", "toggleChatbox");
-    e_6.appendChild(document.createTextNode("折叠"));
-    e_3.appendChild(e_6);
-    e_2.appendChild(e_3);
-    var e_7 = document.createElement("div");
-    e_7.setAttribute("class", "chatbox-body");
-    e_2.appendChild(e_7);
-    e_0.appendChild(e_2);
-    container.appendChild(e_0);
-    return container;
-}
-document.body.appendChild(ChatHtml());
-
-const apiUrl = 'https://chat.zicheng.icu';
-const chatbox = $("#ZichenChatRoomHtmlPluginChatbox");
-const chatboxBody = chatbox.find(".chatbox-body");
-let offset = 0, isCollapsed = true, firstLoad = true;
-
-chatbox.find(".toggleChatbox").click(() => chatbox.toggleClass("collapsed"));
-chatbox.find(".scrollToBottom").click(scrollToBottom);
-
-function sendMessage(message) {
-    if (!(message = message.trim())) return;
-    $.post(`${apiUrl}/api/chat`, { message })
-        .done(response => appendMessage(response.status === "success" ? message : "消息发送失败", "end"))
-        .fail(() => appendMessage("消息发送失败", "end"));
-}
-
-function appendMessage(content, alignment) {
-    chatboxBody.append(`<div class="message-content" style="text-align:${alignment};">${content}</div>`);
-    scrollToBottom();
-}
-
-function scrollToBottom() {
-    chatboxBody.scrollTop(chatboxBody[0].scrollHeight);
-}
-
-function loadMessages() {
-    $.get(`${apiUrl}/api/chat`, { offset })
-        .done(data => populateMessages(data.messages))
-        .fail(() => appendMessage("无法加载聊天记录", "center"));
-}
-
-function populateMessages(messages) {
-    if (messages.length > 0) {
-        offset += messages.length;
-        messages.forEach(({ type, content, user_name }) => {
-            let alignment = type === "user" ? "left" : "right";
-            let userDisplayName = type === "user" ? user_name : "系统";
-            appendMessage(`${userDisplayName}:${content}`, alignment);
+    $(document).ready(function () {
+        $().ZichenChatRoom({
+            apiUrl: 'http://127.0.0.1'
         });
-        if (firstLoad) {
-            scrollToBottom();
-            firstLoad = false;
-        }
-    }
-}
-
-loadMessages();
-setInterval(loadMessages, 3000);
+    });
 </script>
 ```
-
-或者下载仓库中的`chatRoom.min.js`，上传到您的服务器，修改`const apiUrl`值后引入！
-
-如有自定义需求，请相应调整JavaScript代码中的样式或功能。希望这个简易聊天室插件可以给您带来便利。如有任何问题或建议，请随时反馈！
--- chatRoom.js
+++ chatRoom.js
@@ -1,84 +1,206 @@
-function ChatHtml() {
-    var container = document.createDocumentFragment();
-    var e_0 = document.createElement("div");
-    var e_1 = document.createElement("style");
-    e_1.appendChild(document.createTextNode("\n#ZichenChatRoomHtmlPluginChatbox {\nmax-width: 330px;\nheight: 400px;\nborder: 2px solid #ccc;\nbackground-color: #f9f9f9;\nposition: fixed;\nbottom: 50px;\nright: 50px;\nbox-shadow: 0 0 10px rgba(0, 0, 0, 0.2);\nz-index: 1000\n}\n#ZichenChatRoomHtmlPluginChatbox .chatbox-header {\nbox-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);\nbackground: rgba(255, 255, 255, 0.2);\nbackdrop-filter: blur(2.5px);\npadding: 10px;\nuser-select: none\n}\n#ZichenChatRoomHtmlPluginChatbox .chatbox-body {\nmax-height: 88%;\noverflow-y: scroll;\noverflow-x: hidden;\npadding: 10px;\nheight: calc(100% - 70px);\n}\n#ZichenChatRoomHtmlPluginChatbox .chatbox-body .message-content {\npadding: 0.75rem 1rem;\nmargin: 10px;\nborder-radius: 0.75rem;\nword-wrap: break-word;\nbackground-color: #f1f1f1;\n}\n#ZichenChatRoomHtmlPluginChatbox img {\nmax-height: 100%;\nmax-width: 100%\n}\n#ZichenChatRoomHtmlPluginChatbox .chatbox-footer {\npadding: 10px;\nposition: absolute;\nwidth: 100%;\nbottom: 0\n}\n#ZichenChatRoomHtmlPluginChatbox.collapsed .chatbox-body,\n#ZichenChatRoomHtmlPluginChatbox.collapsed .chatbox-footer {\ndisplay: none\n}\n#ZichenChatRoomHtmlPluginChatbox.collapsed {\nheight: 40px\n}\n#ZichenChatRoomHtmlPluginChatbox .chatbox-body,\n#ZichenChatRoomHtmlPluginChatbox .chatbox-footer {\nopacity: 1\n}\n#ZichenChatRoomHtmlPluginChatbox .toggleChatbox,\n#ZichenChatRoomHtmlPluginChatbox .scrollToBottom {\nfloat: right;\nbackground-color: transparent;\nborder: none;\ncursor: pointer;\nfont-size: 16px;\ntransition: transform 0.2s\n}\n@media (max-width:600px) {\n#ZichenChatRoomHtmlPluginChatbox {\nmax-width: 100%;\nwidth: 100%;\nright: 0;\nbottom: 0\n}\n}\n"));
-    e_0.appendChild(e_1);
-    var e_2 = document.createElement("div");
-    e_2.setAttribute("class", "collapsed");
-    e_2.setAttribute("id", "ZichenChatRoomHtmlPluginChatbox");
-    var e_3 = document.createElement("div");
-    e_3.setAttribute("class", "chatbox-header");
-    e_3.appendChild(document.createTextNode("\n简易聊天室\n"));
-    var e_4 = document.createElement("a");
-    e_4.setAttribute("href", "https://chat.zicheng.icu/");
-    e_4.setAttribute("target", "_blank");
-    e_4.setAttribute("rel", "noopener noreferrer");
-    e_4.appendChild(document.createTextNode("完整体验地址"));
-    e_3.appendChild(e_4);
-    var e_5 = document.createElement("button");
-    e_5.setAttribute("class", "scrollToBottom");
-    e_5.appendChild(document.createTextNode("返回底部"));
-    e_3.appendChild(e_5);
-    var e_6 = document.createElement("button");
-    e_6.setAttribute("class", "toggleChatbox");
-    e_6.appendChild(document.createTextNode("折叠"));
-    e_3.appendChild(e_6);
-    e_2.appendChild(e_3);
-    var e_7 = document.createElement("div");
-    e_7.setAttribute("class", "chatbox-body");
-    e_2.appendChild(e_7);
-    e_0.appendChild(e_2);
-    container.appendChild(e_0);
-    return container;
-}
-document.body.appendChild(ChatHtml());
+(function ($) {
+    $.fn.ZichenChatRoom = function (options) {
+        const settings = $.extend({
+            apiUrl: 'http://127.0.0.1', // API URL
+        }, options);
 
-const apiUrl = 'https://chat.zicheng.icu';
-const chatbox = $("#ZichenChatRoomHtmlPluginChatbox");
-const chatboxBody = chatbox.find(".chatbox-body");
-let offset = 0, isCollapsed = true, firstLoad = true;
+        const chatbox = $('<div id="ZichenChatRoomHtmlPluginChatbox">').html(`
+        <style>
+            #ZichenChatRoomHtmlPluginChatbox {
+                max-width: 330px;
+                height: 420px;
+                border: 2px solid #ccc;
+                background-color: #f9f9f9;
+                position: fixed;
+                top: 0;
+                left: 0;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+                z-index: 1000;
+                margin: 10px;
+                transition: all 0.3s linear;
+            }
+            #ZichenChatRoomHtmlPluginChatbox .chatbox-header {
+                box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
+                background: rgba(255, 255, 255, 0.2);
+                backdrop-filter: blur(2.5px);
+                padding: 10px;
+                user-select: none;
+            }
+            #ZichenChatRoomHtmlPluginChatbox .chatbox-body {
+                max-height: 88%;
+                overflow-y: scroll;
+                overflow-x: hidden;
+                height: calc(100% - 70px);
+            }
+            #ZichenChatRoomHtmlPluginChatbox .chatbox-body .message-content {
+                padding: 0.75rem 1rem;
+                margin: 10px;
+                border-radius: 0.75rem;
+                word-wrap: break-word;
+                background-color: #f1f1f1;
+            }
+            #ZichenChatRoomHtmlPluginChatbox img {
+                max-height: 100%;
+                max-width: 100%;
+            }
+            #ZichenChatRoomHtmlPluginChatbox .chatbox-footer {
+                display: flex;
+                position: absolute;
+                width: 100%;
+                flex-direction: column;
+            }
+            #ZichenChatRoomHtmlPluginChatbox.collapsed .chatbox-body,
+            #ZichenChatRoomHtmlPluginChatbox.collapsed .chatbox-footer {
+                display: none;
+            }
+            #ZichenChatRoomHtmlPluginChatbox.collapsed {
+                height: 40px;
+            }
+            #ZichenChatRoomHtmlPluginChatbox.collapsed .scrollToBottom,
+            #ZichenChatRoomHtmlPluginChatbox.collapsed a {
+                display: none;
+            }
+            #ZichenChatRoomHtmlPluginChatbox .chatbox-body,
+            #ZichenChatRoomHtmlPluginChatbox .chatbox-footer {
+                opacity: 1;
+            }
+            #ZichenChatRoomHtmlPluginChatbox .toggleChatbox,
+            #ZichenChatRoomHtmlPluginChatbox .scrollToBottom {
+                background-color: transparent;
+                border: none;
+                cursor: pointer;
+                font-size: 16px;
+                transition: transform 0.2s;
+            }
+            #ZichenChatRoomHtmlPluginChatbox .message-input-container {
+                display: flex;
+            }
+            #ZichenChatRoomHtmlPluginChatbox .message-input-container input {
+                width: 80%;
+                padding: 5px;
+                border: 0px solid #ccc;
+                height: 18px;
+            }
+            #ZichenChatRoomHtmlPluginChatbox .message-input-container button {
+                border: 1px solid #ccc;
+                background-color: #4CAF50;
+                color: white;
+                cursor: pointer;
+            }
+        </style>
+        <div class="chatbox-header">
+            <span>聊天室</span>
+            <button class="toggleChatbox">折叠</button>
+            <a href="https://chat.zicheng.icu/" target="_blank" rel="noopener noreferrer">完整体验地址</a>
+            <button class="scrollToBottom">返回底部</button>
+        </div>
+        <div class="chatbox-body"><span class="loading">加载中……</span></div>
+        <div class="chatbox-footer">
+            <div class="message-input-container">
+                <input type="text" class="message-input" placeholder="请输入消息..." />
+                <button class="sendMessage">发送</button>
+            </div>
+        </div>`);
 
-chatbox.find(".toggleChatbox").click(() => chatbox.toggleClass("collapsed"));
-chatbox.find(".scrollToBottom").click(scrollToBottom);
+        $('body').append(chatbox);
 
-function sendMessage(message) {
-    message = message.trim();
-    if (!message) return;
-    $.post(`${apiUrl}/api/chat`, { message })
-        .done(response => appendMessage(response.status === "success" ? message : "消息发送失败", "end"))
-        .fail(() => appendMessage("消息发送失败", "end"));
-}
+        const tokenManage = function (method) {
+            if (method === 'set') {
+                Cookies.set('ZichenChatRoomHtmlPlugin_Token', chatbox.find('.message-input').val(), { expires: 365 });
+            } else {
+                return Cookies.get('ZichenChatRoomHtmlPlugin_Token');
+            }
+        };
 
-function appendMessage(content, alignment) {
-    chatboxBody.append(`<div class="message-content" style="text-align:${alignment};">${content}</div>`);
-    scrollToBottom();
-}
-
-function scrollToBottom() {
-    chatboxBody.scrollTop(chatboxBody[0].scrollHeight);
-}
-
-function loadMessages() {
-    $.get(`${apiUrl}/api/chat`, { offset })
-        .done(data => populateMessages(data.messages))
-        .fail(() => appendMessage("无法加载聊天记录", "center"));
-}
-
-function populateMessages(messages) {
-    if (messages.length > 0) {
-        offset += messages.length;
-        messages.forEach(({ type, content, user_name }) => {
-            let alignment = type === "user" ? "left" : "right";
-            let userDisplayName = type === "user" ? user_name : "系统";
-            appendMessage(`${userDisplayName}:${content}`, alignment);
-        });
-        if (firstLoad) {
+        const appendMessage = function (content, alignment) {
+            chatboxBody.append(`<div class="message-content" style="text-align:${alignment};">${content}</div>`);
             scrollToBottom();
-            firstLoad = false;
-        }
-    }
-}
+        };
 
-loadMessages();
-setInterval(loadMessages, 3000);
+        const scrollToBottom = function () {
+            chatboxBody.scrollTop(chatboxBody[0].scrollHeight);
+        };
+
+        const loadMessages = function () {
+            $.ajax({
+                type: "POST",
+                url: `${settings.apiUrl}/api/v1/chat/get?offset=${offset}`,
+                data: {
+                    token: token
+                },
+                dataType: "JSON",
+                success: function (response) {
+                    if (response.code === 200) {
+                        tokenIsTrue = true;
+                        populateMessages(response.data.messages);
+                    } else if (response.code === 401) {
+                        appendMessage('请在下方文本框输入token并点发送后刷新网页！', 'center');
+                    } else {
+                        appendMessage(`无法加载聊天记录${response.message}`, 'center');
+                    }
+                },
+                error: function (xhr) {
+                    appendMessage(`无法加载聊天记录 ${xhr.status}`, 'center');
+                }, 
+                complete: function (xhr) {
+                    chatbox.find('.loading').fadeOut();
+                }
+            });
+        };
+
+        const sendMessage = function () {
+            $.ajax({
+                type: "POST",
+                url: `${settings.apiUrl}/api/v1/chat/send`,
+                data: {
+                    token: token,
+                    message: chatbox.find('.message-input').val()
+                },
+                dataType: "JSON",
+                success: function (response) {
+                    if (response.code === 200) {
+                        loadMessages();
+                    } else {
+                        appendMessage(`无法发送消息 ${response.message}`, 'center');
+                    }
+                },
+                error: function (xhr) {
+                    appendMessage(`无法发送消息 ${xhr.status}`, 'center');
+                }
+            });
+        };
+
+        const populateMessages = function (messages) {
+            if (messages.length > 0) {
+                offset += messages.length;
+                messages.forEach(({ type, content, user_name }) => {
+                    let alignment = type === "user" ? "left" : "right";
+                    let userDisplayName = type === "user" ? user_name : "系统";
+                    appendMessage(`${userDisplayName}:${content}`, alignment);
+                });
+            }
+        };
+
+        const chatboxBody = chatbox.find('.chatbox-body');
+        let token = tokenManage();
+        let tokenIsTrue = false;
+        let offset = 0;
+
+        chatbox.find('.toggleChatbox').click(function () {
+            chatbox.toggleClass('collapsed');
+        });
+
+        chatbox.find('.scrollToBottom').click(scrollToBottom);
+
+        chatbox.find('.sendMessage').click(function () {
+            if (tokenIsTrue) {
+                sendMessage();
+            } else {
+                tokenManage('set');
+            }
+        });
+
+        loadMessages();
+        setInterval(loadMessages, 3000);
+    };
+})(jQuery);
